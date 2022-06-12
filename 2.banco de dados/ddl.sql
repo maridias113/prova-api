a4fedff5825d3176318f5663c8b5e42ec97dc525
maridias113 <marianaferreiradias8@gmail.com>
2022-06-12 14:48:00 -0300

    3commit

---
+++ 2.banco de dados/ddl.sql
@@ -0,0 +1,9 @@
+create database PetinfoA;
+
+use PetinfoA;
+
+
+create table tb_pet(
+    id_pet  int primary key auto_increment,
+    nm_pet varchar (50)
+);
---
+++ 2.banco de dados/dml.sql
@@ -0,0 +1,10 @@
+use PetinfoA;
+
+
+ --CSU:01 colocar nome do pet
+ inserir into tb_pet (nm_pet)
+      values ("apolo");
+
+--CSU : 02 buscar todos os nomes 
+
+select * from  tb_pet;

0717e17ecc668b2ec1ec85b73eb0439dfd7742e7
maridias113 <marianaferreiradias8@gmail.com>
2022-06-12 15:51:36 -0300

    5commit

---
+++ api/src/repository/connection.js
@@ -0,0 +1,14 @@
+
+import mysql from 'mysql2/promise.js';
+import 'dotenv/config.js'
+
+const con =mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB
+})
+
+console.log("DB conectado")
+
+export { con }
